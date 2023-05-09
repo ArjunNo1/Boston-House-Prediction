92b6f3f78bd4bcd3e42a3ce5afd8189fe5869e67
Arjun <vishnukumar1105@gmail.com>
2023-05-09 22:16:46 +0530

    Bugs  fixed

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ regression = pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('nav.html')
 
 
 @app.route('/price_prediction',methods = ['POST'])
-def predict():
+def price():
     data = request.json['data']
     inputdata = np.array(list(data.values())).reshape(1,-1)
     scaler  = StandardScaler()
@@ -22,6 +22,16 @@ def predict():
     pred = regression.predict(scaled)
     print(pred[0])
     return jsonify(pred[0])
+
+@app.route('/prediction',methods = ['POST'])
+def pred():
+    data = [float(i) for i in request.form.values()]
+    print(data)
+    scaler = StandardScaler()
+    final_input = scaler.fit_transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output  = regression.predict(final_input)[0]
+    return render_template("index.html",result = "The House price is Estimated to be {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
